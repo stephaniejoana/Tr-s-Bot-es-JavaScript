e85d15c2fb888d8d40911795da4497e887923b50
stephaniejoana <tete.joana.s@gmail.com>
2024-07-28 18:06:14 -0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+document.getElementById('botao1').addEventListener('click', function() {
+    alert('Você clicou no Botão 1');
+});
+
+document.getElementById('botao2').addEventListener('click', function() {
+    alert('Você clicou no Botão 2');
+});
+
+document.getElementById('botao3').addEventListener('click', function() {
+    alert('Você clicou no Botão 3');
+});
